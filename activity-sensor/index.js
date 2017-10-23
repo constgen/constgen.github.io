e228098bdfb02d5a1fde76c881764540a174dfd8
Constantine Genchevsky <const.gen@gmail.com>
2017-10-23 15:38:40 +0300

    Better sensor

--- activity-sensor/index.js
+++ activity-sensor/index.js
@@ -1,34 +1,37 @@
+'use strict';
+
 var ACTIVITY_TYPE = ActivitySensor.ACTIVITY_TYPE;
 var activitySensor = new ActivitySensor();
 activitySensor.subscribe(function (activity) {
-	var activityType = activity.type;
-	var text;
+    var activityType = activity.type;
+    var text;
 
-	switch (activityType) {
-		case 'Idle':
-			text = 'Device is not in hands';
-			break;
-		case 'Fidgeting':
-			text = 'Using device';
-			break;
-		case 'InVehicle':
-			text = 'Using as GPS in vehicle';
-			break;
-		case 'Running':
-			text = 'Running';
-			break;
-		case 'Stationary':
-			text = 'Staying still';
-			break;
-		case 'Walking':
-			text = 'Walking';
-			break;
-		case 'Laying':
-			text = 'In a bed on the back';
-			break;
-		case 'Unknown':
-			text = 'Unknown activity';
-			break;
-	}
-	document.body.innerHTML = '<h1>' + text + '</h1>';
+    switch (activityType) {
+        case ACTIVITY_TYPE.IDLE:
+            text = 'Device is not in hands';
+            break;
+        case ACTIVITY_TYPE.STANDING:
+            text = 'Standing';
+            break;
+        case ACTIVITY_TYPE.SITTING:
+            text = 'Sitting';
+            break;
+        case ACTIVITY_TYPE.IN_VEHICLE:
+            text = 'Using as GPS in vehicle';
+            break;
+        case ACTIVITY_TYPE.RUNNING:
+            text = 'Running';
+            break;
+        case ACTIVITY_TYPE.WALKING:
+            text = 'Walking';
+            break;
+        case ACTIVITY_TYPE.LAYING:
+            text = 'In a bed on the back';
+            break;
+        case ACTIVITY_TYPE.UNKNOWN:
+            text = 'Unknown activity';
+            break;
+    }
+
+    document.body.innerHTML = '<h1>' + text + '</h1>';
 });
